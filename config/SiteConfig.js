bef69ad08c649f47eae64b00d0a96adc8664dc38
Warren <wrn14897@hotmail.com>
2018-12-27 17:01:05 +0800

    deploy process

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: "Warren's Blog", // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
+  siteTitleAlt: '', // Alternative Site title for SEO
   siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.jpg',
