c57a3612548aea3de2b6c6c6dd8172623f0b174a
Warren <wrn14897@hotmail.com>
2019-01-20 10:49:14 -0800

    remove link in SEO

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -84,7 +84,6 @@ const SEO = props => {
       <meta name="twitter:url" content={config.siteUrl} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
-      <link rel="icon" type="image/png" href={config.siteBanner} sizes="32x32" />
     </Helmet>
   );
 };
