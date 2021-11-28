fe25f5566e947c90f66a94a151dffc78827e8e99
Warren <warren@Warrens-MacBook-Pro.local>
2021-11-28 11:37:29 -0800

    bump version

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: "Warren's Blog", // Navigation and Site Title
-  siteTitleAlt: '', // Alternative Site title for SEO
+  siteTitleAlt: "Warren's Blog", // Alternative Site title for SEO
   siteUrl: 'https://www.warrencodes.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.png',
