7a58f9be1a67d59806516ba1dc6bc9996843a533
Warren <5959690+wrn14897@users.noreply.github.com>
2023-05-12 22:49:45 -0700

    feat: finish it up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import netlify from "@astrojs/netlify/functions";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://warrencodes.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   output: "server",
   adapter: netlify()
 });
