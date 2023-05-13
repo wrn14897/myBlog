5c539acbb0520fb580a7856892dec3c8d8531b53
Warren <5959690+wrn14897@users.noreply.github.com>
2023-05-12 23:05:34 -0700

    feat: ignore .netlify

--- .netlify/functions-internal/chunks/third-post.43da6f0f.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./third-post.0ddc15e4.mjs');
-					}
-					const collectedLinks = "@@ASTRO-LINKS@@";
-					const collectedStyles = "@@ASTRO-STYLES@@";
-					const collectedScripts = "@@ASTRO-SCRIPTS@@";
-
-export { collectedLinks, collectedScripts, collectedStyles, getMod };
